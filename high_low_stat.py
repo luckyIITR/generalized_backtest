165e4970f9480c4441307376332f55939024cb56
luckyIITR <luckykushwaha56@gmail.com>
2021-03-03 17:15:55 +0530

    plot in high low stat

--- high_low_stat.py
+++ high_low_stat.py
@@ -8,8 +8,8 @@ from Portfolio2 import BuyPortfolio, Store_Data
 import pandas as pd
 import datetime as dt
 from finta import TA
-
-plt.ioff()
+from plotly.offline import plot
+# plt.ioff()
 
 
 #
@@ -41,6 +41,7 @@ plt.ioff()
 
 
 def get_intra_data(symbol, tp):
+    # daily = yf.download(tickers=symbol, interval="5m",start=dt.datetime(2021,3,3,9,15),end = dt.datetime(2021,3,3,9,40))
     daily = yf.download(tickers=symbol, interval="5m", period=f"{str(tp)}d")
     daily.index = daily.index.tz_localize(None)
     daily.drop(["Adj Close", 'Volume'], axis=1, inplace=True)
@@ -66,21 +67,44 @@ def get_peaks(today):
 
 
 def get_plot(y1, y2, peaks1, peaks2, df):
-    plt.plot(peaks1, y1[peaks1], "x")
-    plt.plot(peaks2, y2[peaks2] * -1, "x")
-    y = df.loc[:, 'Close'].values
-    x = np.array([i for i in range(1, len(y1) + 1)])
-    plt.plot(x - 1, y)
-    for x1 in peaks1:
-        plt.vlines(x=x1, ymin=min(y2 * -1), ymax=max(y1), colors='green', ls=':', lw=1)
-    for x1 in peaks2:
-        plt.vlines(x=x1, ymin=min(y2 * -1), ymax=max(y1), colors='green', ls=':', lw=1)
-
-    for x1 in y1[peaks1]:
-        plt.hlines(y=x1, xmin=min(x), xmax=max(x), colors='red', ls=':', lw=1)
-
-    for x1 in y2[peaks2] * -1:
-        plt.hlines(y=x1, xmin=min(x), xmax=max(x), colors='green', ls=':', lw=1)
+    trace1 = {
+        'x': df.index,
+        'open': df.Open,
+        'close': df.Close,
+        'high': df.High,
+        'low': df.Low,
+        'type': 'candlestick',
+        'name': symbol[:-3],
+        'showlegend': True
+    }
+    trace2 = {
+        'x': df.index[peaks1],
+        'y': y1[peaks1],
+        'type': 'scatter'
+    }
+    trace3 = {
+        'x': df.index[peaks2],
+        'y': y2[peaks2] * -1,
+        'type': 'scatter'
+    }
+    data = [trace1, trace2, trace3]
+    fig = dict(data=data)
+    plot(fig)
+    # plt.plot(df.index[peaks1], y1[peaks1], "x")
+    # plt.plot(df.index[peaks2], y2[peaks2] * -1, "x")
+    # y = df.loc[:, 'Close'].values
+    # x = np.array([i for i in range(1, len(y1) + 1)])
+    # plt.plot(df.index, y)
+    # for x1 in df.index[peaks1]:
+    #     plt.vlines(x=x1, ymin=min(y2 * -1), ymax=max(y1), colors='green', ls=':', lw=1)
+    # for x1 in df.index[peaks2]:
+    #     plt.vlines(x=x1, ymin=min(y2 * -1), ymax=max(y1), colors='green', ls=':', lw=1)
+    #
+    # for x1 in y1[peaks1]:
+    #     plt.hlines(y=x1, xmin=min(df.index), xmax=max(df.index), colors='red', ls=':', lw=1)
+    #
+    # for x1 in y2[peaks2] * -1:
+    #     plt.hlines(y=x1, xmin=min(df.index), xmax=max(df.index), colors='green', ls=':', lw=1)
 
 
 def get_today(df, date):
@@ -89,9 +113,9 @@ def get_today(df, date):
 
 def main(symbol):
     # symbol = "SBIN.NS"
-    backtest_tp = 60
+    backtest_tp = 1
     df_5min = get_intra_data(symbol, backtest_tp)
-    df_hour = get_dates(symbol, backtest_tp)
+    df_hour = get_dates(symbol, backtest_tp+5)
     df_hour['ema21'] = TA.EMA(df_hour, period=21)
     df_hour['ema8'] = TA.EMA(df_hour, period=8)
     df_hour = df_hour.iloc[21:, :].copy()
@@ -121,10 +145,16 @@ def main(symbol):
 
             if port.check_pos() == 1 and e.time() == dt.datetime(2020, 2, 2, 15, 25).time():
                 port.square_off(today.loc[e, 'Open'], e)
-    port.generate_dataframes()
-    store_result.append_data(port.generate_results())
-    store_result.day_wise_result(port.get_day_wise().rename(columns={'%change': f"{symbol[:-3]}"}))
 
+    if len(port.order_book) != 0 :
+        port.generate_dataframes()
+        store_result.append_data(port.generate_results())
+        store_result.day_wise_result(port.get_day_wise().rename(columns={'%change': f"{symbol[:-3]}"}))
+    today = get_today(df_5min, date)
+    df = today
+    y1, y2, peaks1, peaks2 = get_peaks(today)
+    get_plot(y1, y2, peaks1, peaks2, today)
+    # plt.show()
     # port.generate_csv_report()
     # fig = plt.figure(num=None, figsize=(16, 12), dpi=160, facecolor='w', edgecolor='k')
     # plt.plot(port.percent_df['cumprod'], 'bo-')
@@ -135,28 +165,74 @@ def main(symbol):
     # plt.close(fig)
 
 
+
 store_result = Store_Data()
+
+# tickers = ['ADANIPORTS.NS',
+#            'ASIANPAINT.NS',
+#            'AXISBANK.NS',
+#            'BAJAJ-AUTO.NS',
+#            'BAJAJFINSV.NS',
+#            'BAJFINANCE.NS',
+#            'BHARTIARTL.NS',
+#            'BPCL.NS',
+#            'BRITANNIA.NS',
+#            'CIPLA.NS',
+#            'COALINDIA.NS',
+#            'DIVISLAB.NS',
+#            'DRREDDY.NS',
+#            'EICHERMOT.NS',
+#            'GAIL.NS',
+#            'GRASIM.NS',
+#            'HCLTECH.NS',
+#            'HDFC.NS',
+#            'HDFCBANK.NS',
+#            'HDFCLIFE.NS',
+#            'HEROMOTOCO.NS',
+#            'HINDALCO.NS',
+#            'HINDUNILVR.NS',
+#            'ICICIBANK.NS',
+#            'INDUSINDBK.NS',
+#            'INFY.NS',
+#            'IOC.NS',
+#            'ITC.NS',
+#            'JSWSTEEL.NS',
+#            'KOTAKBANK.NS',
+#            'LT.NS',
+#            'M&M.NS',
+#            'MARUTI.NS',
+#            'NESTLEIND.NS',
+#            'NTPC.NS',
+#            'ONGC.NS',
+#            'POWERGRID.NS',
+#            'RELIANCE.NS',
+#            'SBILIFE.NS',
+#            'SBIN.NS',
+#            'SUNPHARMA.NS',
+#            'TATAMOTORS.NS',
+#            'TATASTEEL.NS',
+#            'TCS.NS',
+#            'TECHM.NS',
+#            'TITAN.NS',
+#            'ULTRACEMCO.NS',
+#            'UPL.NS',
+#            'WIPRO.NS']
 
 tickers = ['ADANIPORTS.NS',
            'ASIANPAINT.NS',
            'AXISBANK.NS',
            'BAJAJ-AUTO.NS',
-           'BAJAJFINSV.NS',
-           'BAJFINANCE.NS',
            'BHARTIARTL.NS',
            'BPCL.NS',
-           'BRITANNIA.NS',
            'CIPLA.NS',
            'COALINDIA.NS',
            'DIVISLAB.NS',
            'DRREDDY.NS',
-           'EICHERMOT.NS',
            'GAIL.NS',
            'GRASIM.NS',
            'HCLTECH.NS',
            'HDFC.NS',
            'HDFCBANK.NS',
-           'HDFCLIFE.NS',
            'HEROMOTOCO.NS',
            'HINDALCO.NS',
            'HINDUNILVR.NS',
@@ -169,13 +245,10 @@ tickers = ['ADANIPORTS.NS',
            'KOTAKBANK.NS',
            'LT.NS',
            'M&M.NS',
-           'MARUTI.NS',
-           'NESTLEIND.NS',
            'NTPC.NS',
            'ONGC.NS',
            'POWERGRID.NS',
            'RELIANCE.NS',
-           'SBILIFE.NS',
            'SBIN.NS',
            'SUNPHARMA.NS',
            'TATAMOTORS.NS',
@@ -183,10 +256,10 @@ tickers = ['ADANIPORTS.NS',
            'TCS.NS',
            'TECHM.NS',
            'TITAN.NS',
-           'ULTRACEMCO.NS',
            'UPL.NS',
            'WIPRO.NS']
-# tickers = tickers[:10]
+symbol = tickers[1]
+
 for symbol in tickers:
     main(symbol)
 
@@ -196,3 +269,8 @@ store_result.get_csv()
 # store_result.day_wise
 
 # t = ((day_wise / 100 + 1).cumprod() - 1) * 100
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+
